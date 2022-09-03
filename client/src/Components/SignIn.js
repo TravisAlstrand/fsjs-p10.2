a37ee844d8e24743b080aafb9d8c203cd14227f8
Travis Alstrand <travisalstrand@gmail.com>
2022-09-03 10:48:07 -0700

    created 'api' function and started 'signin'

--- client/src/Components/SignIn.js
+++ client/src/Components/SignIn.js
@@ -1,21 +1,44 @@
-import { Link } from 'react-router-dom';
+import { useState, useContext } from 'react';
+import { Link, Navigate } from 'react-router-dom';
+import { Context } from '../context';
 
 const SignIn = () => {
+
+    const { actions } = useContext(Context);
+
+    const [ emailAddress, setEmailAddress ] = useState('');
+    const [ password, setPassword ] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        // call sign in function
+        actions.signIn(emailAddress, password)
+            .then (response => {
+                if (response !== null) {
+                    Navigate('/');
+                } else {
+                    console.log('sign in failed');
+                };
+            });
+    };
+
     return (
         <main>
             <div className="form--centered">
                 <h2>Sign In</h2>
                 
-                <form>
-                    <label for="emailAddress">Email Address</label>
-                    <input id="emailAddress" name="emailAddress" type="email" value=""></input>
-                    <label for="password">Password</label>
-                    <input id="password" name="password" type="password" value=""></input>
+                <form onSubmit={handleSubmit}>
+                    <label htmlFor="emailAddress">Email Address</label>
+                    <input id="emailAddress" name="emailAddress" type="email" onChange={e => setEmailAddress(e.target.value)} />
+                    <label htmlFor="password">Password</label>
+                    <input id="password" name="password" type="password" onChange={e => setPassword(e.target.value)} />
                     <button className="button" type="submit">Sign In</button>
                     <Link to='/'>
                         <button className="button button-secondary">Cancel</button>
                     </Link>
                 </form>
+
                 <p>Don't have a user account? Click here to <Link to='/signup'>sign up</Link>!</p>
             </div>
         </main>
---
+++ client/src/context.js
@@ -0,0 +1,73 @@
+import React, { useState } from 'react';
+import { Buffer } from 'buffer';
+
+export const Context = React.createContext();
+
+export const Provider = (props) => {
+
+    // state for signed in user
+    const [ user, setUser ] = useState(null);
+
+    // state for username
+    const [ authedUsername, setAuthedUsername ] = useState('');
+
+    // state for password
+    const [ authedUserPassword, setAuthedUserPassword ] = useState('');
+
+    // function for all api requests
+    function api(path, method = 'GET', body = null, requiresAuth = false, credentials = null) {
+        // base url
+        const url = 'http://localhost:5000/api' + path;
+
+        const options = {
+            method,
+            headers: {
+                'Content-Type': 'application/json; charset=utf-8'
+            }
+        };
+
+        if (body !== null) {
+            options.body = JSON.stringify(body);
+        }
+
+        if (requiresAuth) {
+            const encryptedCredentials = Buffer.from(`${credentials.username}:${credentials.password}`).toString('base64');
+            options.headers['Authorization'] = `Basic ${encryptedCredentials}`;
+        }
+
+        const results = fetch(url, options);
+
+        return (results);
+    };
+
+    /* ========================================== */
+    /* -------------- USER API CALLS ------------ */
+    /* ========================================== */
+
+    // funtion to GET user info & sign user in
+    async function handleSignIn(username, password) {
+        const response = await api('/users', 'GET', null, true, {username, password});
+
+        if (response.status === 200) {
+            setAuthedUsername(username);
+            setAuthedUserPassword(password);
+            return response.json()
+                .then(data => setUser(data));
+        } else if (response.status === 401) {
+            return null;
+        } else {
+            throw new Error();
+        }
+    }
+
+    return (
+      <Context.Provider value={{
+        user,
+        actions: {
+            signIn: handleSignIn
+        }
+      }}>
+        {props.children}
+      </Context.Provider>  
+    );
+};
